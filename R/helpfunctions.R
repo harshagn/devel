ac29b985b64c2308cb0c21e80e5a62fb11e08cf7
dluedecke <d.luedecke@uke.de>
2014-09-02 10:47:17 +0200

    bugfix

--- R/helpfunctions.R
+++ R/helpfunctions.R
@@ -145,7 +145,7 @@ sju.modsum.lm <- function(fit) {
     substitute(italic(b[0]) == a * "," ~~ R^2 == r2 * "," ~~ "adj. " * R^2 == ar2 * "," ~~ "F" == f*panval * "," ~~ "AIC" == aic,
                list(a=format(coef(fit)[1], digits=3),
                     r2=format(summary(fit)$r.squared, digits=3),
-                    r2=format(summary(fit)$adj.r.squared, digits=3),
+                    ar2=format(summary(fit)$adj.r.squared, digits=3),
                     f=sprintf("%.2f", fstat[1]),
                     panval=pan,
                     aic=sprintf("%.2f", AIC(fit))))))
